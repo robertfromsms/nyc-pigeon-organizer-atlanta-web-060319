c48d87ffb0efe46354ee64a2dd29282a66f54ce0
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-20 01:53:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  all_pigeon = []
+  data.each {|traits, chars|
+    chars.each {|char, piegons|
+      all_pigeon.concat(piegons)
+    }
+  }
+  all_pigeon.uniq!
+  reformat_pigeon_data = {}
+  all_pigeon.each {|pigeon|
+    reformat_pigeon_data[pigeon] = {}
+    data.each {|traits, chars|
+      reformat_pigeon_data[pigeon][traits] = []
+      chars.each {|char, pigeon_array|
+        if pigeon_array.include?(pigeon)
+          reformat_pigeon_data[pigeon][traits].push(char.to_s)
+        end
+      }
+    }
+  }
+  return reformat_pigeon_data
 end
