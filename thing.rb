645989d601f364af8c41a1a9239cfdb986558bfa
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-20 01:53:48 +0000

    Done.

---
+++ thing.rb
@@ -0,0 +1,29 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+def thing(data)
+  data.each {|traits, chars|
+    chars.each {|char, piegons|
+      puts char
+      puts piegons
+    }
+  }
+end
+
+thing(pigeon_data)
